27d3d77de332a6394f7b6d0060261bdd49d32280
ManuelCorera <manucorera10@gmail.com>
2022-10-02 21:43:59 -1200

    Primeros Cambios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; //importa libreria de react. - sin barra lo busca en node_modules
+import ReactDOM from 'react-dom/client';//importa al encargado del Virtual Dom y el Dom.
+import './index.css';//importa los estilos del index. no es necesario el nombre con archivos css
+import App from './App';//importa el otro js "App.js". con punto barra busca en src
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); //busca el root del html y le pinta el codigo de abajo
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
